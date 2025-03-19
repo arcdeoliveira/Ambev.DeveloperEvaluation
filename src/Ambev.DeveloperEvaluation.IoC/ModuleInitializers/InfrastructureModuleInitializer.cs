df2423b026052bd92069a3c58dc7b88a0b8a7901
arcdeoliveira <arcdeoliveira@gmail.com>
2025-03-19 19:39:30 -0300

    Update the calling of the namespace path.

--- src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
+++ src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
@@ -1,9 +1,8 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
-using Ambev.DeveloperEvaluation.ORM;
-using Ambev.DeveloperEvaluation.ORM.Repositories;
+﻿using Ambev.DeveloperEvaluation.Domain.Interfaces.Repositories.RelationalDatabase;
+using Ambev.DeveloperEvaluation.ORM.Contexts;
+using Ambev.DeveloperEvaluation.ORM.Repositories.RelationalDatabase;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers;
@@ -12,7 +11,7 @@ public class InfrastructureModuleInitializer : IModuleInitializer
 {
     public void Initialize(WebApplicationBuilder builder)
     {
-        builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<DefaultContext>());
+        builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<PostgreContext>());
         builder.Services.AddScoped<IUserRepository, UserRepository>();
     }
 }
